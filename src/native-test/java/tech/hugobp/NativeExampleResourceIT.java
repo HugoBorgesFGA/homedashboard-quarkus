6e1bcf63269eb8ea7e63856168939c9e9638fa8a
Hugo Pinto <hugobp14@hotmail.com>
2020-06-05 20:12:44 +0100

    Initial commit - Generated at code.quarkus.io

---
+++ src/native-test/java/tech/hugobp/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package tech.hugobp;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
